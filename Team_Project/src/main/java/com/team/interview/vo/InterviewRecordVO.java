8c86a11429e0c4cb116882632986808c8d7f5557
Kim Changu <cgkim449@gmail.com>
2021-09-08 04:51:21 +0900

    refactor: InterviewRecordVO에 answerVOList 필드 추가

--- Team_Project/src/main/java/com/team/interview/vo/InterviewRecordVO.java
+++ Team_Project/src/main/java/com/team/interview/vo/InterviewRecordVO.java
@@ -1,91 +1,32 @@
 package com.team.interview.vo;
 
 import java.sql.Date;
+import java.util.List;
 
 public class InterviewRecordVO {
 
+  // id(pk)
   private int iRecordId;
+  // 멘토 이메일(fk)
   private String mentorEmail;
+  // 면접자 이메일(fk)
   private String email;
+  // 면접 유형 id(fk)
   private int iTypeId;
+  // 선택한 키워드
   private String chosenKw;
+  // 선택한 직군
   private String chosenPos;
+  // 등록일
   private Date regdate;
+  // 수정일
   private Date updatedate;
 
+  private String type;
+  private List<AnswerVO> answerVOList;
+
+
   public InterviewRecordVO() {
-  }
-
-  protected boolean canEqual(Object other) {
-    return other instanceof MemberVO;
-  }
-
-  @Override
-  public String toString() {
-    return "InterviewRecordVO [iRecordId=" + iRecordId + ", mentorEmail=" + mentorEmail + ", email="
-        + email + ", iTypeId=" + iTypeId + ", chosenKw=" + chosenKw + ", chosenPos=" + chosenPos
-        + ", regdate=" + regdate + ", updatedate=" + updatedate + "]";
-  }
-
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((chosenKw == null) ? 0 : chosenKw.hashCode());
-    result = prime * result + ((chosenPos == null) ? 0 : chosenPos.hashCode());
-    result = prime * result + ((email == null) ? 0 : email.hashCode());
-    result = prime * result + iRecordId;
-    result = prime * result + iTypeId;
-    result = prime * result + ((mentorEmail == null) ? 0 : mentorEmail.hashCode());
-    result = prime * result + ((regdate == null) ? 0 : regdate.hashCode());
-    result = prime * result + ((updatedate == null) ? 0 : updatedate.hashCode());
-    return result;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    InterviewRecordVO other = (InterviewRecordVO) obj;
-    if (chosenKw == null) {
-      if (other.chosenKw != null)
-        return false;
-    } else if (!chosenKw.equals(other.chosenKw))
-      return false;
-    if (chosenPos == null) {
-      if (other.chosenPos != null)
-        return false;
-    } else if (!chosenPos.equals(other.chosenPos))
-      return false;
-    if (email == null) {
-      if (other.email != null)
-        return false;
-    } else if (!email.equals(other.email))
-      return false;
-    if (iRecordId != other.iRecordId)
-      return false;
-    if (iTypeId != other.iTypeId)
-      return false;
-    if (mentorEmail == null) {
-      if (other.mentorEmail != null)
-        return false;
-    } else if (!mentorEmail.equals(other.mentorEmail))
-      return false;
-    if (regdate == null) {
-      if (other.regdate != null)
-        return false;
-    } else if (!regdate.equals(other.regdate))
-      return false;
-    if (updatedate == null) {
-      if (other.updatedate != null)
-        return false;
-    } else if (!updatedate.equals(other.updatedate))
-      return false;
-    return true;
   }
 
   public int getiRecordId() {
@@ -151,5 +92,22 @@ public class InterviewRecordVO {
   public void setUpdatedate(Date updatedate) {
     this.updatedate = updatedate;
   }
+
+  public List<AnswerVO> getAnswerVOList() {
+    return answerVOList;
+  }
+
+  public void setAnswerVOList(List<AnswerVO> answerVOList) {
+    this.answerVOList = answerVOList;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
 
 }
